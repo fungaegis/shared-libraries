3893d9c75f2c58163513c2142d4191d6015349e7
fungaegis <fungaegis@gmail.com>
2020-10-27 15:35:24 +0800

    2

--- vars/pipelineTemplate.groovy
+++ vars/pipelineTemplate.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 // library 'share-library'
-import com.unitTest
+// import com.unitTest
 
 def call(String type,Map map) {
     if (type == "golang") {
@@ -13,7 +13,7 @@ def call(String type,Map map) {
             stages {
                 stage('单元测试') {
                     steps{
-                        unitTest.call
+                        unitTest()
                     }
                 }
             }
