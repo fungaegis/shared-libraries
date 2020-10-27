00b70643600563606f78043c8024dbd89b753b4f
fungaegis <fungaegis@gmail.com>
2020-10-27 17:18:53 +0800

    18

--- vars/pipelineTemplate.groovy
+++ vars/pipelineTemplate.groovy
@@ -11,7 +11,6 @@ def call(String type,Map map) {
                 stage('单元测试') {
                     steps{
                         unitTest(server) // 可以直接使用同一个package里面的东西
-                        echo(libraryResource 'hello_world.json')
                         // log.info('xixixixixixixi' + server)  // Method calls on objects not allowed outside "script" blocks
                         script {
                             log.info('xixixixixixixi' + server)
--- vars/unitTest.groovy
+++ vars/unitTest.groovy
@@ -3,4 +3,6 @@
 def call(String server){
     echo 'unittesting...............' + server
     echo server
+    def res = LibraryResource 'hello_world.json'
+    echo res
 }
